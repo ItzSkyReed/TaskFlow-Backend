632b61e0ce30059d5565afaa466a2de2ca6b0382
SkyReed <itzskyreed@gmail.com>
2025-08-04 01:35:33 +0500

    feat: упрощен паттерн пароля

--- src/auth/constants.py
+++ src/auth/constants.py
@@ -2,8 +2,6 @@ import re
 from typing import Final
 
 LOGIN_PATTERN: Final[re.Pattern] = re.compile(r"^[a-zA-Z0-9_]+$")
-PASSWORD_PATTERN: Final[re.Pattern] = re.compile(
-    r"^[A-Za-z\d_!@#$%^&*()\-=\[\]{};:'\",.<>/?\\|`~]*$"
-)
+PASSWORD_PATTERN: Final[re.Pattern] = re.compile(r"^[A-Za-z\d!@#$%&*]*$")
 
 MAX_REFRESH_TOKENS: Final[int] = 5
