45f69abdf5858ace11b3952956600cc9c27af0cd
SkyReed <itzskyreed@gmail.com>
2025-08-12 15:25:20 +0500

    refactor: добавлен асинхронный менеджер контекста для get_minio_client

--- src/minio/client.py
+++ src/minio/client.py
@@ -1,20 +1,23 @@
 from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
 
 import aioboto3
+from aiobotocore.client import BaseClient
 
 from .. import get_settings
 
 settings = get_settings()
 
 
-async def get_minio_client() -> AsyncGenerator:
+@asynccontextmanager
+async def get_minio_client() -> AsyncGenerator[BaseClient, None]:
     """
     :return: Асинхронный S3 клиент для работы с minio
     """
     session = aioboto3.Session()
     async with session.client(
         service_name="s3",
-        endpoint_url=f"minio:{settings.minio_storage_port}",
+        endpoint_url=f"http://minio:{settings.minio_storage_port}",
         aws_access_key_id=settings.minio_root_user,
         aws_secret_access_key=settings.minio_root_password,
         use_ssl=False,
