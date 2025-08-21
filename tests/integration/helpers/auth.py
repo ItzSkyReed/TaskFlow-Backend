ef100128d23e7408e61f8d48251b5dbfa6b7a7bb
SkyReed <itzskyreed@gmail.com>
2025-08-21 15:37:41 +0500

    refactor: улучшена типизация тестов

--- tests/integration/helpers/auth.py
+++ tests/integration/helpers/auth.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import TypedDict
 
 from httpx import AsyncClient
 from starlette import status
@@ -6,25 +7,36 @@ from starlette import status
 from src.auth import auth_router
 
 
-async def register_and_login(
-    client: AsyncClient, unique=None
-) -> dict[str, str | dict[str, str]]:
+class PayloadDict(TypedDict):
+    name: str
+    login: str
+    email: str
+    password: str
+
+
+class AuthResult(TypedDict):
+    payload: PayloadDict
+    refresh_token: str
+    access_token: str
+
+
+async def register_and_login(client: AsyncClient, unique=None) -> AuthResult:
     if unique is None:
         unique = uuid.uuid4().hex[:16]
 
-    payload = {
-        "name": f"User{unique}",
-        "login": f"user{unique}",
-        "email": f"user{unique}@example.com",
-        "password": unique,
-    }
+    payload = PayloadDict(
+        name=f"User{unique}",
+        login=f"user{unique}",
+        email=f"user{unique}@example.com",
+        password=unique,
+    )
 
     # Регистрация
     response = await client.post(f"{auth_router.prefix}/sign_up", json=payload)
     assert response.status_code == status.HTTP_201_CREATED
 
-    refresh_token = response.cookies.get("refresh_token")
-    access_token = response.json().get("access_token")
+    refresh_token = response.cookies["refresh_token"]
+    access_token = response.json()["access_token"]
 
     return {
         "payload": payload,
