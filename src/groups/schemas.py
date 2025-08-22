152c6266ee92de6ecfaa924e7dad239d926554a9
SkyReed <itzskyreed@gmail.com>
2025-08-22 22:16:20 +0500

    refactor: Схемы групп разделены на детальные и summary

--- src/groups/schemas.py
+++ src/groups/schemas.py
@@ -30,8 +30,28 @@ class CreateGroupSchema(BaseModel):
         ),
     ]
 
+class GroupSummarySchema(BaseModel):
+    id: UUID
 
-class GroupSchema(BaseModel):
+    name: Annotated[str, Field(max_length=50)]
+
+    has_avatar: Annotated[bool, Field(default=False, exclude=True)]
+
+    creator_id: UUID
+
+    created_at: Annotated[datetime, Field(...)]
+
+    @computed_field
+    @property
+    def avatar_url(self) -> str | None:
+        # Проверяем наличие атрибута has_avatar и что он True
+        if getattr(self, "has_avatar", False):
+            return f"{settings.cdn_path}/avatars/groups/{self.id}.webp"
+        return None
+
+    model_config = ConfigDict(from_attributes=True)
+
+class GroupDetailSchema(BaseModel):
     id: UUID
 
     name: Annotated[str, Field(max_length=50)]
