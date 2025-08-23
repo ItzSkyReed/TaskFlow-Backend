d1b9eb256d59232ce8ba084afe70551b2b93ce4d
SkyReed <itzskyreed@gmail.com>
2025-08-23 20:23:41 +0500

    chore: импортированы все модели из groups

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
 # Импорт всех моделей для корректной работы миграций и самих моделей
 
 from .user import User, UserProfile  # noqa: F401
+from .groups import Group, GroupMember, GroupPermission, GroupUserPermission, GroupInvitation, GroupJoinRequest, InvitationStatus, JoinRequestStatus # noqa: F401
