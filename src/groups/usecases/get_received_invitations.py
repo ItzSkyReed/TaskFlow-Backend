acaf5a080d9eca4d8451eb66ad47120f3e522516
SkyReed <itzskyreed@gmail.com>
2025-08-28 15:04:29 +0500

    feat: get_received_invitations обновлен для использования новой GroupSummarySchema

--- src/groups/usecases/get_received_invitations.py
+++ src/groups/usecases/get_received_invitations.py
@@ -6,8 +6,10 @@ from sqlalchemy.orm import joinedload
 
 from ...user import User
 from ..enums import InvitationStatus
-from ..models import GroupInvitation
-from ..schemas import ReceivedInvitationSchema
+from ..models import GroupInvitation, Group
+from ..schemas import GroupInvitationSchema
+from ..services import get_groups_member_count, get_groups_user_context
+from ..services.group_service import map_to_group_invitation_schema
 
 
 async def get_received_invitations(
@@ -16,21 +18,20 @@ async def get_received_invitations(
     offset: int,
     invitee_id: UUID,
     session: AsyncSession,
-) -> list[ReceivedInvitationSchema]:
+) -> list[GroupInvitationSchema]:
     """
     Получение списка приглашений с фильтрацией
     :param offset: Смещение от начала выборки
     :param limit: Лимит возвращаемых значений за раз
     :param invitation_status: Статусы заявок
-    :param invitee_id: ID группы
+    :param invitee_id: ID приглашенного пользователя
     :param session: Сессия
     """
 
     stmt = (
         select(GroupInvitation)
         .options(
-            joinedload(GroupInvitation.group),
-            joinedload(GroupInvitation.inviter).joinedload(User.user_profile),
+            joinedload(GroupInvitation.group).selectinload(Group.users).joinedload(User.user_profile)
         )
         .where(GroupInvitation.invitee_id == invitee_id)
         .order_by(GroupInvitation.created_at.desc())
@@ -40,11 +41,21 @@ async def get_received_invitations(
     if invitation_status:
         stmt = stmt.where(GroupInvitation.status.in_(invitation_status))
 
-    result = (await session.execute(stmt)).scalars().all()
+    invitations = (await session.execute(stmt)).scalars().all()
 
-    # Преобразуем в Pydantic-схему
-    invitations = [
-        ReceivedInvitationSchema.model_validate(inv, from_attributes=True)
-        for inv in result
+    groups_seq = [invitation.group for invitation in invitations]
+
+    groups_user_context_map = await get_groups_user_context(
+        groups_seq, invitee_id, session
+    )
+
+    members_count = await get_groups_member_count(groups_seq, session)
+
+    return [
+        map_to_group_invitation_schema(
+            invitation,
+            groups_user_context_map[invitation.group.id],
+            members_count[invitation.group.id],
+        )
+        for invitation in invitations
     ]
-    return invitations
