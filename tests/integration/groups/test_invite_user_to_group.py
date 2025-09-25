114af5b5669e1a2cae945988e265df9728142f59
SkyReed <itzskyreed@gmail.com>
2025-09-25 18:40:44 +0500

    test: добавлены тесты для отправки заявки пользователю

---
+++ tests/integration/groups/test_invite_user_to_group.py
@@ -0,0 +1,140 @@
+import uuid
+
+import pytest
+from httpx import AsyncClient
+from integration.helpers import add_user_to_group, create_group, register_and_login, set_authorization
+from starlette import status
+
+from src.groups import group_router
+
+
+async def test_invite_user_success(client: AsyncClient):
+    user = await register_and_login(client)
+    user2 = await register_and_login(client)
+    await set_authorization(client, user)
+
+    created_group_response = await create_group(client)
+
+    assert created_group_response.status_code == status.HTTP_201_CREATED
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user2["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_201_CREATED
+
+
+async def test_invite_user_invite_yourself(client: AsyncClient):
+    user = await register_and_login(client)
+    await set_authorization(client, user)
+
+    created_group_response = await create_group(client)
+
+    assert created_group_response.status_code == status.HTTP_201_CREATED
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_400_BAD_REQUEST
+    assert invite_response.json()["detail"][0]["type"] == "group.invitation.cannot_invite_yourself"
+
+
+async def test_invite_user_no_group(client: AsyncClient):
+    user = await register_and_login(client)
+    user2 = await register_and_login(client)
+    await set_authorization(client, user)
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{uuid.uuid4()}/invitations",
+        json={"user_id": user2["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_404_NOT_FOUND
+    assert invite_response.json()["detail"][0]["type"] == "group.not_found"
+
+
+async def test_invite_user_no_user(client: AsyncClient):
+    user = await register_and_login(client)
+    await set_authorization(client, user)
+    created_group_response = await create_group(client)
+
+    assert created_group_response.status_code == status.HTTP_201_CREATED
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{uuid.uuid4()}/invitations",
+        json={"user_id": str(uuid.uuid4())},
+    )
+    assert invite_response.status_code == status.HTTP_404_NOT_FOUND
+    assert invite_response.json()["detail"][0]["type"] == "group.not_found"
+
+
+async def test_invite_user_not_found(client: AsyncClient):
+    user = await register_and_login(client)
+    await set_authorization(client, user)
+    created_group_response = await create_group(client)
+
+    assert created_group_response.status_code == status.HTTP_201_CREATED
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{uuid.uuid4()}/invitations",
+        json={"user_id": str(uuid.uuid4())},
+    )
+    assert invite_response.status_code == status.HTTP_404_NOT_FOUND
+    assert invite_response.json()["detail"][0]["type"] == "group.not_found"
+
+
+@pytest.mark.order(after="test_respond_to_invitation.py::test_invitation_members_limit")
+async def test_invite_user_already_invited(client: AsyncClient):
+    user = await register_and_login(client)
+    user2 = await register_and_login(client)
+    await set_authorization(client, user)
+    created_group_response = await create_group(client)
+
+    invite_response = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user2["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_201_CREATED
+
+    invite_response_2 = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user2["id"]},
+    )
+    assert invite_response_2.status_code == status.HTTP_201_CREATED
+
+    assert invite_response.json()["created_at"] == invite_response_2.json()["created_at"]
+
+
+@pytest.mark.order(after="test_respond_to_invitation.py::test_invitation_members_limit")
+async def test_invite_user_already_in_group(client: AsyncClient):
+    user = await register_and_login(client)
+    user2 = await register_and_login(client)
+    await set_authorization(client, user)
+    created_group_response = await create_group(client)
+
+    await add_user_to_group(client, user, user2, created_group_response.json()["id"])
+
+    await set_authorization(client, user)
+    invite_response = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user2["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_400_BAD_REQUEST
+    assert invite_response.json()["detail"][0]["type"] == "group.invitation.invited_user_already_in_group"
+
+
+@pytest.mark.order(after="test_respond_to_invitation.py::test_invitation_members_limit")
+async def test_invite_user_no_permissions(client: AsyncClient):
+    user = await register_and_login(client)
+    user2 = await register_and_login(client)
+    user3 = await register_and_login(client)
+    await set_authorization(client, user)
+    created_group_response = await create_group(client)
+    await add_user_to_group(client, user, user2, created_group_response.json()["id"])
+
+    await set_authorization(client, user2)
+    invite_response = await client.post(
+        f"{group_router.prefix}/{created_group_response.json()['id']}/invitations",
+        json={"user_id": user3["id"]},
+    )
+    assert invite_response.status_code == status.HTTP_403_FORBIDDEN
